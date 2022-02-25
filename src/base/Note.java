b2ba993d1d071babf3c454a9a71b7f03910107c9
kelvinkk577 <kelvinkk577@gmail.com>
2022-02-25 18:10:04 +0800

    completed lab2

---
+++ src/base/Note.java
@@ -0,0 +1,27 @@
+package base;
+
+import java.util.Date;
+
+public class Note {
+	private Date date;
+	private String title;
+	
+	public Note(String title) {
+		this.title = title;
+		date = new Date(System.currentTimeMillis());
+	}
+	
+	public String getTitle() {
+		return title;
+	}
+	
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (!(obj instanceof Note))
+			return false;
+		Note other = (Note) obj;
+		return title.equals(other.getTitle());
+	}	
+}
