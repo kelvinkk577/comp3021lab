b109ccfca0461eb0ef1d4952974f37c932015fbc
kelvinkk577 <kelvinkk577@gmail.com>
2022-04-29 19:49:16 +0800

    lab9 complete

---
+++ src/lab9/FindMax.java
@@ -0,0 +1,101 @@
+package lab9;
+
+/**
+ * 
+ * COMP 3021
+ * 
+This is a class that prints the maximum value of a given array of 90 elements
+
+This is a single threaded version.
+
+Create a multi-thread version with 3 threads:
+
+one thread finds the max among the cells [0,29] 
+another thread the max among the cells [30,59] 
+another thread the max among the cells [60,89]
+
+Compare the results of the three threads and print at console the max value.
+
+ * 
+ * @author valerio
+ *
+ */
+public class FindMax {
+	// this is an array of 90 elements
+	// the max value of this array is 9999
+	static int[] array = { 1, 34, 5, 6, 343, 5, 63, 5, 34, 2, 78, 2, 3, 4, 5, 234, 678, 543, 45, 67, 43, 2, 3, 4543,
+			234, 3, 454, 1, 2, 3, 1, 9999, 34, 5, 6, 343, 5, 63, 5, 34, 2, 78, 2, 3, 4, 5, 234, 678, 543, 45, 67, 43, 2,
+			3, 4543, 234, 3, 454, 1, 2, 3, 1, 34, 5, 6, 5, 63, 5, 34, 2, 78, 2, 3, 4, 5, 234, 678, 543, 45, 67, 43, 2,
+			3, 4543, 234, 3, 454, 1, 2, 3 };
+
+	public static void main(String[] args) {
+		new FindMax().printMax();
+	}
+	
+	class Task implements Runnable
+	{
+		private int begin, end, result;
+		
+		public Task(int begin, int end)
+		{
+			this.begin = begin;
+			this.end = end;
+		}
+		
+		@Override
+		public void run() {
+			// TODO Auto-generated method stub
+			result = findMax(begin, end);
+		}
+		
+		public int get()
+		{
+			return result;
+		}
+		
+	}
+	
+	public void printMax() {
+		Task tk1 = new Task(0, 29);
+		Task tk2 = new Task(30, 59);
+		Task tk3 = new Task(60, 89);
+		Thread thread1 = new Thread(tk1);
+		Thread thread2 = new Thread(tk2);
+		Thread thread3 = new Thread(tk3);
+		thread1.start();
+		thread2.start();
+		thread3.start();
+		try {
+			thread1.join();
+			thread2.join();
+			thread3.join();
+		} catch (InterruptedException e)
+		{
+			e.printStackTrace();
+		}
+		int max = tk1.get();
+		if (tk2.get() > max)
+			max = tk2.get();
+		if (tk3.get() > max)
+			max = tk3.get();
+		System.out.println("the max value is " + max);
+	}
+
+	/**
+	 * returns the max value in the array within a give range [begin,range]
+	 * 
+	 * @param begin
+	 * @param end
+	 * @return
+	 */
+	private int findMax(int begin, int end) {
+		// you should NOT change this function
+		int max = array[begin];
+		for (int i = begin + 1; i <= end; i++) {
+			if (array[i] > max) {
+				max = array[i];
+			}
+		}
+		return max;
+	}
+}
